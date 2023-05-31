67c472f8a0de03c97ab4e161f8aa583df18523b5
HenriqueAngar <devhenriqueangar@gmail.com>
2023-05-31 17:10:49 -0300

    init developing engine

---
+++ aplication/src/app/engine/engine.js
@@ -0,0 +1,41 @@
+
+//ENGINE MAIN FUNCTION
+//Control data operations in aplication
+
+import world from './worldTools'
+
+export default function startEngine() {
+
+    //INIT ENGINE FUNCTIONS
+
+    let userData = manageStart(0, 'Rique', null)
+    userData.worlds.push(new world('Terra', -10000, 2023))
+
+    console.log(userData)
+}
+
+function manageStart(id, name, text) {
+
+    //CONTROL INFORMATION GENERATION ACCORDING USERDATA
+
+    if (text == null) {
+
+        let data = new dataFrame(id, name)
+        return data
+
+    } else {
+
+        //create information from JSON
+    }
+}
+class dataFrame {
+
+    //CREATE DATA FRAME FROM USER DATA
+
+    constructor(id, name) {
+
+        this.id = id,
+            this.name = name,
+            this.worlds = []
+    }
+}
---
+++ aplication/src/app/engine/worldTools.js
@@ -0,0 +1,15 @@
+
+export default class world {
+
+    //OBJECT CLASS FOR WORLD
+
+    constructor(name, init, end) {
+
+        this.name = name,
+            this.init = init,
+            this.end = end,
+            this.models = [],
+            this.conections = [],
+            this.lists = []
+    }
+}
