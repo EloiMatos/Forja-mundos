afc6eb9d4e94cad923835e6239da82ec94bcb430
LorenzoDalBo <lorenzotrevizoldalbo@gmail.com>
2023-06-05 22:25:06 -0300

    git update

--- aplication/src/app/page.tsx
+++ aplication/src/app/page.tsx
@@ -1,11 +1,11 @@
-import { url } from "inspector";
+
 import startEngine from "./engine/engine.js";
 import React from "react";
 export default function Home() {
   startEngine();
 
   return (
-    <main className="h-screen flex w-screen">
+    <main className="h-screen flex w-screen bg-cover bg-no-repeat bg-[url('../../public/image5.svg')]">
       <form className="flex justify-center w-2/5 ml-4 h-full flex-col">
         <div className="h-5/6 flex gap-2 bg-white bg-opacity-25 rounded-lg justify-center flex-col">
         <strong className="flex text-3xl justify-center mb-2">Login</strong>
