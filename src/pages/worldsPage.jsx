bac1048bf158cca1dab47ddd80d92bdd056273f4
lucasborba111 <lucasborgesborba@gmail.com>
2023-07-01 23:14:13 -0300

    Arrumando css, que estava fazendo h-screen

--- src/pages/worldsPage.jsx
+++ src/pages/worldsPage.jsx
@@ -1,26 +1,15 @@
 import LeftDrawer from "@/components/LeftDrawer";
 import { EastContainer } from "../components/East/EastContainer"
-import { Alert, AlertTitle } from "@mui/material";
-import { useEffect } from "react";
 import { useState } from "react";
 import {Container} from "@mui/material";
-import  Header  from "@/components/Header";
+import { Navbar } from "@/components/Navbar";
 
 function WorldsPage() {
     const [isDrawerOpen, setIsDrawerOpen] = useState(false)
 
-    useEffect(() =>{
-        <span>
-            <Alert severity="info">
-                <AlertTitle>É HORA DO SHOW PORRA!</AlertTitle>
-            </Alert>
-        </span>
-
-    }, [])
-
     return(
-        <main className="pb-20 items-center w-full h-screen bg-indigo-500 ">
-            <Header/>
+        <main className="pb-20 items-center w-full h-full bg-indigo-500 ">
+            <Navbar/>
             <Container 
                 maxWidth={false} 
                 className='h-full'
